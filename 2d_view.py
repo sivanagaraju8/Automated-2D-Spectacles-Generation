2dfa3b369e570989860f8818805cfc04af398d30
Gamidi Siva Nagaraju <31251886+sivanagaraju8@users.noreply.github.com>
2021-03-28 22:33:21 +0530

    Update 2d_view.py
    comments adjustments

--- 2d_view.py
+++ 2d_view.py
@@ -10,15 +10,15 @@
 import numpy as np
 import cv2
 from PIL import Image
-height =256 #height of the image
-width = 512	#width of the image
+height =256 	#height of the image
+width = 512 	#width of the image
 
 print("**** Enter Details for 2D View of Spectacles (in cm)****")
 lens= input("Enter type of lens (round/rectangle): ") 
 if lens=="round":
 	diameter=int(float(input("Enter diameter: "))*100)
 elif lens=="rectangle":
-	 len_height=int(float(input("Enter height of glass: ")))*100	#Lens height
+	 len_height=int(float(input("Enter height of glass: ")))*100		#Lens height
 	 len_width=int(float(input("Enter Width of glass: ")))*100		#Lens width
 else:
 	print("Error: please enter the availiable lens type")
@@ -32,31 +32,31 @@ t=input("Enter frame color (prefer hex): ").lstrip('#')
 l= input("Enter Lens color (prefer hex): ").lstrip('#')
 
 t_color=tuple(int(t[i:i+2], 16) for i in (0, 2, 4))	#conversion of hex color to RGB
-l_color=tuple(int(l[i:i+2], 16) for i in (0, 2, 4)) #conversion of hex color to RGB	
+l_color=tuple(int(l[i:i+2], 16) for i in (0, 2, 4)) 	#conversion of hex color to RGB	
 t_color=t_color[::-1]	#reversimg the color in BGR format
 l_color=l_color[::-1]	#reversing the color in BGR format
 #img1=cv2.imread("../specs.png",1)
 
-img=np.ones((height,width,3),np.uint8)*255 #create an empty image 
+img=np.ones((height,width,3),np.uint8)*255 	#create an empty image 
 if lens=='round':
-	radius=int(diameter/2)		#radius of the glass
-	centre=int(height/2)		#centre point of height 
+	radius=int(diameter/2)			#radius of the glass
+	centre=int(height/2)			#centre point of height 
 	ref=centre-20				#some reference in height (Y)
-	bcx1=(width//2)-(b_w//2)	#brdge X coordinate1
-	bcx2=(width//2)+(b_w//2)	#brdge X coordinate2
+	bcx1=(width//2)-(b_w//2)		#brdge X coordinate1
+	bcx2=(width//2)+(b_w//2)		#brdge X coordinate2
 	bcy1=ref-(b_h//2)-8			#brdge Y coordinate1
 	bcy2=ref+(b_h//2)-8			#brdge Y coordinate2
 	ecx1=bcx1-radius			#elipse1 X cordinate
 	ecy1=centre-15				#elipse1 Y coordinate
-	ecx2=bcx2+radius            #elipse2 X Coordinate 
+	ecx2=bcx2+radius            		#elipse2 X Coordinate 
 	ecy2=centre-15				#elipse2 Y Coordinate 
-	tempx1= ecx1-5-(radius+8)   #Left Temple holder coordiante's Distance
-	tempx2= ecx2+5+(radius+8)   #Right Temple holder coordinate's Distance 
+	tempx1= ecx1-5-(radius+8)   		#Left Temple holder coordiante's Distance
+	tempx2= ecx2+5+(radius+8)   		#Right Temple holder coordinate's Distance 
 
 	temp1 = np.array([[[0,ref-int(t_h/2)],[tempx1,ref-int(t_h/2)-5],		# temple holder cordinates
 	 			[tempx1,ref+int(t_h/2)+5],[0,ref+int(t_h/2)]]], np.int32) 
 
-	temp2 = np.array([[[width-1,ref-int(t_h/2)],[tempx2,ref-int(t_h/2)-5], 	# temple holder cordinates
+	temp2 = np.array([[[width-1,ref-int(t_h/2)],[tempx2,ref-int(t_h/2)-5], 		# temple holder cordinates
 			[tempx2,ref+int(t_h/2)+5],[width-1,ref+int(t_h/2)]]], np.int32)
 
 	img = cv2.fillPoly(img, [temp1], t_color)		#draw temple holder1
@@ -75,10 +75,10 @@ if lens=='round':
 	cv2.imwrite("final_2d_out.png",img)				#save image
 else:
 
-	centre=int(height/2) 		#centre point of height
+	centre=int(height/2) 			#centre point of height
 	ref=centre-10		 		#some reference point
-	bcx1=(width//2)-(b_w//2)	#bridge X cordinate1
-	bcx2=(width//2)+(b_w//2)	#bridge X Coordinate2 
+	bcx1=(width//2)-(b_w//2)		#bridge X cordinate1
+	bcx2=(width//2)+(b_w//2)		#bridge X Coordinate2 
 	bcy1=ref-(b_h//2)			#bridge Y Coordinate1
 	bcy2=ref+(b_h//2)			#bridge Y Coordinate2 
 
@@ -114,7 +114,7 @@ datas = img2.getdata()							#get all info from converted image
 newData = []
 for item in datas:
     if item[0] == 255 and item[1] == 255 and item[2] == 255:	#adding alpha channel and
-        newData.append((255, 255, 255, 0))						#making it transparent
+        newData.append((255, 255, 255, 0))			#making it as transparent
 
     #reducing the alpha value of glass's color 
     elif item[0] ==l_color[2] and item[1]==l_color[1] and item[2]==l_color[0]:
